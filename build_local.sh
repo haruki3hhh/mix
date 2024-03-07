912791ad8a0fe36d4bb240178d4e4506d153b045
haruki3hhh <135201297+haruki3hhh@users.noreply.github.com>
2024-03-07 09:28:56 -0600

    Create build_local.sh

---
+++ build_local.sh
@@ -0,0 +1,27 @@
+git clone https://github.com/srcML/srcSAX.git
+git clone https://github.com/srcML/srcML.git
+git clone https://github.com/srcML/nameCollector.git
+
+# first build srcML
+pushd srcML
+git reset --hard origin/develop
+mkdir build
+cmake . --preset ubuntu-release -B build/
+ninja -C build/
+ninja -C build/ install
+popd
+
+
+pushd srcSAX
+mkdir build
+cmake . -B build/
+make -j`nproc` -C build/
+make -C build/ install
+popd
+
+pushd nameCollector
+mkdir build
+cmake . -B build/
+make -j`nproc` -C build/
+file build/bin/nameCollector
+popd
